9e862fcfcb71512d6c7652b51a28e938f9b524f5
Marco Fagnani <marcofagnani54@gmail.com>
2026-05-15 19:05:48 +0200

    add: Added files to start programs on the cluster. Better late than never.

---
+++ cluster_spawn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=bestEffort
+#SBATCH --output=my_output_%j.out
+#SBATCH --error=my_error_%j.err
+#SBATCH --partition=edu-short
+#SBATCH --account=gpu.computing26
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+module load cuda/12.4
+./execute.sh
